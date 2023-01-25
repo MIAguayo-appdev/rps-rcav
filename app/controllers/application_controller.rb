dc86edd8c101578bf0f200b7e9cd7835bc1e76b2
MIAguayo <maguayo@chicagobooth.edu>
2023-01-25 15:27:10 +0000

    Working RPS with @s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,51 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-def play_rock
-  #write a ton of code
-    
-    #redirect_to("https://www.wikipedia.org")
-    
-    #render({:render=>rand(100)})
+  def homepage 
+    render({:template => "game_templates/rules.html.erb"})
+  end
+  
+  
+  def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
 
-    #render({:html=>"<h1>Hello,World!</h1>"})  ### if you dont include html safe it renders the character as they are
-    
-    #render({:html=>"<h1>Hello,World!</h1>".html_safe})
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
+
     render({:template => "game_templates/user_rock.html.erb"})
+  end
 
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({:template => "game_templates/user_paper.html.erb"})
+  end
+
+  
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    
+    render({:template => "game_templates/user_scissors.html.erb"})
   end
 end
