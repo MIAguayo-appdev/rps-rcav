3bc5c687d2af6c808fc55d6dc75517f79173c115
MIAguayo <maguayo@chicagobooth.edu>
2023-01-25 14:39:29 +0000

    commit 1: first dynamic output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+def play_rock
+  #write a ton of code
+    
+    #redirect_to("https://www.wikipedia.org")
+    
+    #render({:render=>rand(100)})
+
+    #render({:html=>"<h1>Hello,World!</h1>"})  ### if you dont include html safe it renders the character as they are
+    
+    #render({:html=>"<h1>Hello,World!</h1>".html_safe})
+   
+    render({:template => "game_templates/user_rock.html.erb"})
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
+  get("/rock",{:controller=> "application",:action => "play_rock"})
 
 end
+
+
+
+#  get("",{:controller=> "",:action => ""}) ---- format for new routes
+#  get("/WHATWEWANT",{:controller=> "APPLICATION",:action => "METHOD"}) 
